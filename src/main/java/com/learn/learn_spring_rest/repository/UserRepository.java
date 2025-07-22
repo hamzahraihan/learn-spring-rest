4950842e6e3e5762c62838129e9b7b5d2c5b5f4c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-07-22 14:27:09 +0700

    Add UserRepository interface for user data access

---
+++ src/main/java/com/learn/learn_spring_rest/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.learn.learn_spring_rest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.learn.learn_spring_rest.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
