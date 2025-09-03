5de5b2a1711a72f11f0b91b61f17ab9b24b59827
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-03 15:58:59 +0700

    Add AuthController for user authentication login endpoint

---
+++ src/main/java/com/learn/learn_spring_rest/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.learn.learn_spring_rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learn.learn_spring_rest.model.LoginUserRequest;
+import com.learn.learn_spring_rest.model.TokenResponse;
+import com.learn.learn_spring_rest.model.WebResponse;
+import com.learn.learn_spring_rest.service.AuthService;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+public class AuthController {
+  @Autowired
+  private AuthService authService;
+
+  @PostMapping(path = "/api/auth/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public WebResponse<TokenResponse> login(@RequestBody LoginUserRequest request) {
+    TokenResponse tokenResponse = authService.login(request);
+
+    return WebResponse.<TokenResponse>builder().data(tokenResponse).build();
+  }
+
+}
