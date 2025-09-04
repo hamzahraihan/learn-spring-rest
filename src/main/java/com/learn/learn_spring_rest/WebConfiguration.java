c9b7a6ee0e230961cc5b9b33e46e5a122232fbae
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-04 08:51:27 +0700

    Add WebConfiguration to configure argument resolvers for user authentication

---
+++ src/main/java/com/learn/learn_spring_rest/WebConfiguration.java
@@ -0,0 +1,25 @@
+package com.learn.learn_spring_rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.learn.learn_spring_rest.resolver.UserArgumentResolver;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+  @Autowired
+  private UserArgumentResolver userArgumentResolver;
+
+  @Override
+  public void addArgumentResolvers(@NonNull List<HandlerMethodArgumentResolver> resolvers) {
+    WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+    resolvers.add(userArgumentResolver);
+  }
+
+}
