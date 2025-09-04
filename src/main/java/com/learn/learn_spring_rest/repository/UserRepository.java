35b3966cbe98b1ce5ff111db6e41720f8e667925
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-04 08:51:42 +0700

    Add method to find the first user by token in UserRepository

--- src/main/java/com/learn/learn_spring_rest/repository/UserRepository.java
+++ src/main/java/com/learn/learn_spring_rest/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.learn.learn_spring_rest.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.learn.learn_spring_rest.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-
+  Optional<User> findFirstByToken(String token);
 }
