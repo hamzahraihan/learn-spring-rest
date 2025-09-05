34eda029aaf9a403dba28bb810eab53b34ada7f4
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-05 16:19:38 +0700

    Add update method in UserService for user profile updates

--- src/main/java/com/learn/learn_spring_rest/service/UserService.java
+++ src/main/java/com/learn/learn_spring_rest/service/UserService.java
@@ -1,5 +1,7 @@
 package com.learn.learn_spring_rest.service;
 
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -7,6 +9,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.learn.learn_spring_rest.entity.User;
 import com.learn.learn_spring_rest.model.RegisterUserRequest;
+import com.learn.learn_spring_rest.model.UpdateUserRequest;
 import com.learn.learn_spring_rest.model.UserResponse;
 import com.learn.learn_spring_rest.repository.UserRepository;
 import com.learn.learn_spring_rest.security.BCrypt;
@@ -42,6 +45,22 @@ public class UserService {
 
   public UserResponse get(User user) {
     return UserResponse.builder().username(user.getUsername()).name(user.getName()).build();
+  }
+
+  @Transactional
+  public UserResponse update(User user, UpdateUserRequest request) {
+    validationService.validate(request);
+    if (Objects.nonNull(request.getName())) {
+      user.setName(request.getName());
+    }
+
+    if (Objects.nonNull(request.getPassword())) {
+      user.setPassword(BCrypt.hashpw(request.getPassword(), BCrypt.gensalt()));
+    }
+
+    userRepository.save(user);
+
+    return UserResponse.builder().name(user.getName()).username(user.getUsername()).build();
   }
 
 }
