a6c8d930ad0928d2ec9c776f06f2b609bd0a3121
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-05 16:19:50 +0700

    Add update user tests to UserControllerTest for unauthorized and success scenarios

--- src/test/java/com/learn/learn_spring_rest/controller/UserControllerTest.java
+++ src/test/java/com/learn/learn_spring_rest/controller/UserControllerTest.java
@@ -16,6 +16,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.learn.learn_spring_rest.entity.User;
 import com.learn.learn_spring_rest.model.RegisterUserRequest;
+import com.learn.learn_spring_rest.model.UpdateUserRequest;
 import com.learn.learn_spring_rest.model.UserResponse;
 import com.learn.learn_spring_rest.model.WebResponse;
 import com.learn.learn_spring_rest.repository.UserRepository;
@@ -172,6 +173,63 @@ class UserControllerTest {
               new TypeReference<WebResponse<String>>() {
               });
           assertNotNull(response.getErrors());
+        });
+  }
+
+  @Test
+  void updateUserUnauthorized() throws Exception {
+    UpdateUserRequest request = new UpdateUserRequest();
+
+    mockMvc.perform(
+        patch("/api/users/current")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+        .andExpectAll(
+            status().isUnauthorized())
+        .andDo(result -> {
+          WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(),
+              new TypeReference<WebResponse<String>>() {
+              });
+          assertNotNull(response.getErrors());
+        });
+  }
+
+  @Test
+  void updateUserSuccess() throws Exception {
+    User user = new User();
+    user.setName("test");
+    user.setUsername("test");
+    user.setToken("test");
+    user.setPassword(BCrypt.hashpw("test", BCrypt.gensalt()));
+    user.setTokenExpiredAt(System.currentTimeMillis() + 1000000000L);
+
+    userRepository.save(user);
+
+    UpdateUserRequest request = new UpdateUserRequest();
+    request.setName("John");
+    request.setPassword("test123");
+
+    mockMvc.perform(
+        patch("/api/users/current")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request))
+            .header("X-API-TOKEN", "test"))
+        .andExpectAll(
+            status().isOk())
+        .andDo(result -> {
+          WebResponse<UserResponse> response = objectMapper.readValue(result.getResponse().getContentAsString(),
+              new TypeReference<WebResponse<UserResponse>>() {
+              });
+          assertNull(response.getErrors());
+          assertEquals("John", response.getData().getName());
+          assertEquals("test", response.getData().getUsername());
+
+          // check if user data has been updated inside database
+          User userDB = userRepository.findById("test").orElse(null);
+          assertNotNull(userDB);
+          assertTrue(BCrypt.checkpw("test123", userDB.getPassword()));
         });
   }
 
