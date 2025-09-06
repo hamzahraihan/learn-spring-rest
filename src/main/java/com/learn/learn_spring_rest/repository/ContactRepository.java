edcf73e4cb81c8f8c91ad8fe26169f2b8dc443a3
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-06 19:17:46 +0700

    Add method to find contact by user and ID in ContactRepository

--- src/main/java/com/learn/learn_spring_rest/repository/ContactRepository.java
+++ src/main/java/com/learn/learn_spring_rest/repository/ContactRepository.java
@@ -1,9 +1,13 @@
 package com.learn.learn_spring_rest.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.learn.learn_spring_rest.entity.Contact;
+import com.learn.learn_spring_rest.entity.User;
 
 public interface ContactRepository extends JpaRepository<Contact, String> {
+  Optional<Contact> findFirstByUserAndId(User user, String id);
 
 }
