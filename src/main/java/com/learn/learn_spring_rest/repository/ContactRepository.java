9dd35acaa88b10c46f3acdb87360be9afbe7a7ba
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-07 16:01:11 +0700

    Add JpaSpecificationExecutor to ContactRepository for enhanced query capabilities

--- src/main/java/com/learn/learn_spring_rest/repository/ContactRepository.java
+++ src/main/java/com/learn/learn_spring_rest/repository/ContactRepository.java
@@ -3,11 +3,12 @@ package com.learn.learn_spring_rest.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.learn.learn_spring_rest.entity.Contact;
 import com.learn.learn_spring_rest.entity.User;
 
-public interface ContactRepository extends JpaRepository<Contact, String> {
+public interface ContactRepository extends JpaRepository<Contact, String>, JpaSpecificationExecutor<Contact> {
   Optional<Contact> findFirstByUserAndId(User user, String id);
 
 }
