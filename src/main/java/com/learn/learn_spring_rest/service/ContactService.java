6e1ce683d8c20ee06598fe0eabf7f8d4cd108e2b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-07 16:01:15 +0700

    Implement update and delete methods in ContactService, and enhance search functionality with specifications

--- src/main/java/com/learn/learn_spring_rest/service/ContactService.java
+++ src/main/java/com/learn/learn_spring_rest/service/ContactService.java
@@ -1,8 +1,16 @@
 package com.learn.learn_spring_rest.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,7 +20,11 @@ import com.learn.learn_spring_rest.entity.Contact;
 import com.learn.learn_spring_rest.entity.User;
 import com.learn.learn_spring_rest.model.ContactResponse;
 import com.learn.learn_spring_rest.model.CreateContactRequest;
+import com.learn.learn_spring_rest.model.SearchContactRequest;
+import com.learn.learn_spring_rest.model.UpdateContactRequest;
 import com.learn.learn_spring_rest.repository.ContactRepository;
+
+import jakarta.persistence.criteria.Predicate;
 
 @Service
 public class ContactService {
@@ -48,6 +60,66 @@ public class ContactService {
     return toContactResponse(contact);
   }
 
+  @Transactional
+  public ContactResponse update(User user, UpdateContactRequest request) {
+    validationService.validate(request);
+
+    Contact contact = contactRepository.findFirstByUserAndId(user, request.getId())
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Contact not found!"));
+
+    contact.setFirstName(request.getFirstName());
+    contact.setLastName(request.getLastName());
+    contact.setEmail(request.getEmail());
+    contact.setPhone(request.getPhone());
+    contactRepository.save(contact);
+
+    return toContactResponse(contact);
+  }
+
+  @Transactional
+  public void delete(User user, String contactId) {
+    Contact contact = contactRepository.findFirstByUserAndId(user, contactId)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Contact not found!"));
+
+    contactRepository.delete(contact);
+  }
+
+  @SuppressWarnings("null")
+  @Transactional(readOnly = true)
+  public Page<ContactResponse> search(User user, SearchContactRequest request) {
+    Specification<Contact> specification = (root, query, builder) -> {
+      List<Predicate> predicates = new ArrayList<>();
+      predicates.add(builder.equal(root.get("user"), user));
+
+      if (Objects.nonNull(request.getName())) {
+        predicates.add(builder.or(
+            builder.like(root.get("firstName"), "%" + request.getName() + "%"),
+            builder.like(root.get("lastName"), "%" + request.getName() + "%")));
+      }
+
+      if (Objects.nonNull(request.getEmail())) {
+        predicates.add(builder.or(
+            builder.like(root.get("email"), "%" + request.getEmail() + "%")));
+      }
+
+      if (Objects.nonNull(request.getPhone())) {
+        predicates.add(builder.or(
+            builder.like(root.get("phone"), "%" + request.getPhone() + "%")));
+      }
+
+      return query.where(predicates.toArray(new Predicate[] {})).getRestriction();
+    };
+
+    Pageable pageable = PageRequest.of(request.getPage(), request.getSize());
+
+    Page<Contact> contacts = contactRepository.findAll(specification, pageable);
+    List<ContactResponse> contactResponses = contacts.getContent().stream()
+        .map(this::toContactResponse)
+        .toList();
+
+    return new PageImpl<>(contactResponses, pageable, contacts.getTotalElements());
+  }
+
   private ContactResponse toContactResponse(Contact contact) {
     return ContactResponse.builder()
         .id(contact.getId())
@@ -57,4 +129,5 @@ public class ContactService {
         .phone(contact.getPhone())
         .build();
   }
+
 }
