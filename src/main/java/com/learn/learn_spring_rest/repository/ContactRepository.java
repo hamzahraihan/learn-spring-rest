6d910bbf6555be31e3ad189fcc03edd66c264581
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-08 12:27:41 +0700

    Add missing @Repository annotation to ContactRepository interface

--- src/main/java/com/learn/learn_spring_rest/repository/ContactRepository.java
+++ src/main/java/com/learn/learn_spring_rest/repository/ContactRepository.java
@@ -4,10 +4,12 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import com.learn.learn_spring_rest.entity.Contact;
 import com.learn.learn_spring_rest.entity.User;
 
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, String>, JpaSpecificationExecutor<Contact> {
   Optional<Contact> findFirstByUserAndId(User user, String id);
 
