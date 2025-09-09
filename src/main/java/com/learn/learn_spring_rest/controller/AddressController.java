636e252557d552e1c2fcc1be3327d768e1f2ed76
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-09 09:02:02 +0700

    Enhance AddressController with JSON media type specifications for create and get address endpoints

--- src/main/java/com/learn/learn_spring_rest/controller/AddressController.java
+++ src/main/java/com/learn/learn_spring_rest/controller/AddressController.java
@@ -8,9 +8,11 @@ import com.learn.learn_spring_rest.model.CreateAddressRequest;
 import com.learn.learn_spring_rest.model.WebResponse;
 import com.learn.learn_spring_rest.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class AddressController {
@@ -18,7 +20,7 @@ public class AddressController {
   @Autowired
   private AddressService addressService;
 
-  @PostMapping(path = "/api/contacts/{contactId}/addresses")
+  @PostMapping(path = "/api/contacts/{contactId}/addresses", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
   public WebResponse<AddressResponse> create(User user, @RequestBody CreateAddressRequest request,
       @PathVariable("contactId") String contactId) {
 
@@ -26,6 +28,14 @@ public class AddressController {
     AddressResponse addressResponse = addressService.create(user, request);
 
     return WebResponse.<AddressResponse>builder().data(addressResponse).build();
+  }
+
+  @GetMapping(path = "/api/contacts/{contactId}/addresses/{addressId}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public WebResponse<AddressResponse> get(User user, @PathVariable("contactId") String contactId,
+      @PathVariable("addressId") String addressId) {
+    AddressResponse response = addressService.get(user, contactId, addressId);
+
+    return WebResponse.<AddressResponse>builder().data(response).build();
   }
 
 }
