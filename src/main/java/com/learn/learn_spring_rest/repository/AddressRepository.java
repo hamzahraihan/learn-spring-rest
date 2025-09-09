2fce6d99efb8285bf18a041343717ca37251ecd8
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-09 09:02:21 +0700

    Add method to find address by contact and ID in AddressRepository

--- src/main/java/com/learn/learn_spring_rest/repository/AddressRepository.java
+++ src/main/java/com/learn/learn_spring_rest/repository/AddressRepository.java
@@ -1,11 +1,16 @@
 package com.learn.learn_spring_rest.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.learn.learn_spring_rest.entity.Address;
+import com.learn.learn_spring_rest.entity.Contact;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, String> {
+
+  Optional<Address> findFirstByContactAndId(Contact contact, String id);
 
 }
